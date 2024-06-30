74b77c1f05b492eff5f7d9b2a1dd64af40fc18ed
Atharv Pandey <72992789+AtharvPandey@users.noreply.github.com>
2024-06-30 16:58:38 +0530

    Update data.js

--- client/src/constant/data.js
+++ client/src/constant/data.js
@@ -62,7 +62,7 @@ export const bannerData = [
   },
   {
     id: 5,
-    url: "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/e1c1d7c8f5398b28.jpg?q=80",
+    url: "https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/e1c1d7c8f5398b28.jpg?q=80",
   },
   {
     id: 6,
